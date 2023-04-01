015be5f6c35ee8bf2782000d69a11c6de4ad7735
alessandrofoglia07 <alessandrofoglia07@gmail.com>
2023-04-01 16:37:11 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# OpenAI with NodeJS BACKEND
+Configuration for chat completion using gpt-3.5
+
+-- You'll need a OpenAI API key
+    Visit https://openai.com/blog/openai-api for more
+
+-- You'll need an .env file with this structure
+    OPENAI_API_KEY="YOUR-OPENAI-KEY"
---
+++ dist/index.js
@@ -0,0 +1,35 @@
+import express from 'express';
+import cors from 'cors';
+import { Configuration, OpenAIApi } from "openai";
+import * as dotenv from 'dotenv';
+dotenv.config();
+const app = express();
+app.use(cors());
+app.use(express.json());
+const port = 3002;
+app.listen(port, () => { console.log(`Server running on port ${port}`); });
+const config = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(config);
+app.get('/api/completion', async (req, res) => {
+    try {
+        const prompt = req.query.prompt;
+        console.log(prompt);
+        const completion = await openai.createCompletion({
+            model: 'gpt-3.5-turbo',
+            prompt: prompt,
+        });
+        res.send(completion.data.choices[0].text);
+        console.log('res sent');
+    }
+    catch (error) {
+        if (error.response) {
+            console.log(error.response.data);
+            console.log(error.response.status);
+        }
+        else {
+            console.log(error.message);
+        }
+    }
+});
